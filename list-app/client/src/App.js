04d1d75631c78baf69dfe5fc5182b64adcbc10a8
tejal-bhavsar-au6 <tejalbhavsar02@gmail.com>
2020-09-26 15:50:09 +0530

    initial commit

---
+++ list-app/client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import ListsContainer from './ListsContainer';
+
+function App() {
+  return (
+    <div className="App">
+    <header className="App-header">
+      <h1 className="App-title">Hello Hiplyst!</h1>
+    </header>
+    <ListsContainer />
+  </div>
+  );
+}
+
+export default App;
---
+++ list-app/list-app/db/migrate/20200925095439_create_lists.rb
@@ -0,0 +1,11 @@
+class CreateLists < ActiveRecord::Migration[6.0]
+  def change
+    create_table :lists do |t|
+      t.string :title
+      t.text :excerpt
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
