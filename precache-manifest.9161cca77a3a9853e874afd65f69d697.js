3f2792c463d3703a7c5dc276b534bc42a8840499
tejal-bhavsar-au6 <tejalbhavsar02@gmail.com>
2020-11-01 11:49:00 +0530

    Updates

---
+++ precache-manifest.9161cca77a3a9853e874afd65f69d697.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6163219b6205361001e9516f90d3338f",
+    "url": "/tejal-bhavsar-au6/faceboon-client/index.html"
+  },
+  {
+    "revision": "9baac48c615c84a84bba",
+    "url": "/tejal-bhavsar-au6/faceboon-client/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "330399da357fadf77f88",
+    "url": "/tejal-bhavsar-au6/faceboon-client/static/css/main.008e539c.chunk.css"
+  },
+  {
+    "revision": "9baac48c615c84a84bba",
+    "url": "/tejal-bhavsar-au6/faceboon-client/static/js/2.b040fc9a.chunk.js"
+  },
+  {
+    "revision": "abfa9001b4087bb9e9a892e4db297d32",
+    "url": "/tejal-bhavsar-au6/faceboon-client/static/js/2.b040fc9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "330399da357fadf77f88",
+    "url": "/tejal-bhavsar-au6/faceboon-client/static/js/main.26794a24.chunk.js"
+  },
+  {
+    "revision": "8e2e7e5dbc4d482a9a42",
+    "url": "/tejal-bhavsar-au6/faceboon-client/static/js/runtime-main.6f243a47.js"
+  }
+]);
